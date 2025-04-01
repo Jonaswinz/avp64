9ab22875f391628102832e116776ae5b9eb693a8
Jonaswinz <jonas.winzer@rwth-aachen.de>
2025-04-01 11:33:14 +0200

    Adding license for testing code.

--- src/testing/mmio_probe.cpp
+++ src/testing/mmio_probe.cpp
@@ -1,3 +1,22 @@
+/*
+* Copyright (C) 2025 ICE RWTH-Aachen
+*
+* This file is part of AFL++ VP-Mode.
+*
+* AFL++ VP-Mode is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* AFL++ VP-Mode is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with AFL++ VP-Mode. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "testing/mmio_probe.h"
 
 namespace testing {
